43d05d0390c2c1a6852c4ab77494a44285ee6ee4
Imcber <imcber1@gmail.com>
2020-06-17 13:06:47 -0500

    deleted dependecies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { ApolloServer, makeExecutableSchema } from "apollo-server";
+import { ApolloServer, makeExecutableSchema } from "apollo-server-express";
 import { resolvers } from "./db/resolver";
 import { typeDefs } from "./db/schema";
 import { accountsServer } from "./config/db";
@@ -7,9 +7,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-toolkit/schema-merging";
 import { AccountsModule } from "@accounts/graphql-api";
 import User from "./db/resolvers/User";
 
-//connect db
-//connectDB();
-
+const app = express();
 //Generate the account-js module
 const accountsGraphQL = AccountsModule.forRoot({ accountsServer });
 
@@ -20,12 +18,13 @@ const schema = makeExecutableSchema({
   schemaDirectives: { ...accountsGraphQL.schemaDirectives },
 });
 
+server.applyMiddleware({ app });
 const server = new ApolloServer({
   schema,
   context: accountsGraphQL.context,
 });
 
-server.listen({ port: 4000 }, () => {
+app.listen({ port: 4000 }, () => {
   console.log(
     `Server is running in http://localhost:4000${server.graphqlPath}`
   );
